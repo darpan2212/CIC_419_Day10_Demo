ed7bbe0a0553092323f84dbe16eb3115bfbcc24c
Darpan Madlani <Darpan@proclink.com>
2021-12-18 09:26:47 +0530

    three variable initialised with random two digit number

--- sortingArray.sh
+++ sortingArray.sh
@@ -1,6 +1,12 @@
-read -p "Enter the value of a: " a;
-read -p "Enter the value of b: " b;
-read -p "Enter the value of c: " c;
+#read -p "Enter the value of a: " a;
+#read -p "Enter the value of b: " b;
+#read -p "Enter the value of c: " c;
+
+a=$((RANDOM%90+10));
+b=$((RANDOM%90+10));
+c=$((RANDOM%90+10));
+
+echo $a $b $c;
 
 expr[0]=$(($a+$b*$c));
 expr[1]=$(($a*$b+$c));
