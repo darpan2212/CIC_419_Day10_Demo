01d199d984177c55028567f466d74503c8a4cbf0
Darpan Madlani <Darpan@proclink.com>
2021-12-18 10:03:22 +0530

    sorting an array in descending order

--- sortingArray.sh
+++ sortingArray.sh
@@ -1,16 +1,29 @@
-#read -p "Enter the value of a: " a;
-#read -p "Enter the value of b: " b;
-#read -p "Enter the value of c: " c;
 
+#generating 3 random numbers in place of taking user input
 a=$((RANDOM%90+10));
 b=$((RANDOM%90+10));
 c=$((RANDOM%90+10));
 
-echo $a $b $c;
+echo "Random numbers : " $a $b $c;
 
 expr[0]=$(($a+$b*$c));
 expr[1]=$(($a*$b+$c));
 expr[2]=$(($c+$a/$b));
 expr[3]=$(($a%$b+$c));
 
-echo ${expr[@]};
+echo "Before sorting an array : "${expr[@]};
+
+for ((i=0;i<${#expr[@]};i++))
+do
+	for ((j=i+1;j<${#expr[@]};j++))
+	do
+		if [ ${expr[$i]} -lt ${expr[$j]} ]
+		then
+			temp=${expr[$i]};
+			expr[$i]=${expr[$j]};
+			expr[$j]=$temp;
+		fi
+	done
+done
+
+echo "After sorting an array (Descending) : "${expr[@]};
---
+++ swapVariable.sh
@@ -0,0 +1,10 @@
+a=5;
+b=6;
+
+echo "Before swaping : $a $b";
+
+temp=$a;
+a=$b;
+b=$temp;
+
+echo "After swaping : $a $b";
