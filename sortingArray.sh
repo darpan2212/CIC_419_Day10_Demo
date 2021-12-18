84dccc72da5cdfeacd44359e4d296965eda9a74b
Darpan Madlani <Darpan@proclink.com>
2021-12-18 09:21:57 +0530

    computations stored with an array

--- sortingArray.sh
+++ sortingArray.sh
@@ -3,5 +3,8 @@ read -p "Enter the value of b: " b;
 read -p "Enter the value of c: " c;
 
 expr[0]=$(($a+$b*$c));
+expr[1]=$(($a*$b+$c));
+expr[2]=$(($c+$a/$b));
+expr[3]=$(($a%$b+$c));
 
-echo ${expr[0]};
+echo ${expr[@]};
